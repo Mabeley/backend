0310ed2ac326b84e0569bd0100f5b52e9e3aee79
Mabeley <mabel.elcie@gmail.com>
2022-03-17 10:56:58 -0500

    semana01

---
+++ Dia1/03-operadores-aritmeticos.py
@@ -0,0 +1,24 @@
+numero1 = 10
+numero2=20
+
+#suma
+print(numero1 + numero2)
+#resta
+print(numero1 - numero2)
+#multiplicacion
+print(numero1 * numero2)
+#division 
+print(numero1 / numero2)
+#modulo
+print(numero1 % numero2)
+#cociente
+print(numero1//numero2)
+
+
+nombre = 'eduardo'
+ciudad = 'arequipa'
+#para la su,a al ser str se hara la concatenacion (se fusionara los valores)
+print(nombre+ciudad)
+
+#en el caso de la multiplicacion al ser entre un str y int lo que se hara se mostara la cantidad de veces repetidas indicadas en el int del str
+print(nombre* 10)
---
+++ Dia1/04-operadores-asignacion.py
@@ -0,0 +1,21 @@
+#OPERADORES DE ASIGNACION
+numero1, numero2 = 10,20
+#IGUAL =
+numero1 = 10
+print(numero1)
+#INCREMENTO
+numero1 +=1  #el print solo sirve para imprimir valores mas no para hacer procesos de asignacion
+#DECREMENTO
+numero1 -=1
+#MULTIPLICADOR
+numero1*=2
+print(numero1)
+#DIVIDIDENDO
+numero1/=2
+print(numero1)
+#POTENCIA
+numero1 **=2
+
+
+#PARA DESPLAZAR UNA LINEA:
+#seleccionar la linea o las lineas : Alt + flechas direccionales
