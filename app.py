b51125dcdd16cb58263ad12fd2629ebf3f3d34ec
Mabeley <mabel.elcie@gmail.com>
2022-03-18 14:07:06 -0500

    primeros pasos usando el ORM

--- app.py
+++ app.py
@@ -2,11 +2,31 @@ from flask import Flask
 from datetime import datetime
 from flask_restful import Api
 from controllers.ingredientes import IngredientesController
+from config import conexion
 
 app = Flask(__name__)
 #creamos la instancia de flask_restfull.Api y le indicamos toda
 #la configuracion que haremos se agrege a nuestra instancia de Flask
 api= Api(app=app)
+
+#aap.config > acac se alamcenara todas las variables de configuracion de mi proyecto flask, en ella se podran encontrar algunas variables como DEBUG y ENV
+#APP.config ES UN DICCIONARIO EN EL CUAL SE ALMACENARAN LAS VARIABLES POR LLAVE: VALOR
+print(app.config)
+
+
+#APP.config> SE ASIGNA LA CADENA DE CONEXION A NUESTRA BASE DATOS
+#tipo://usuario:password@dominio:puerto/base_de_datos
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:H4ku@127.0.0.1:3306/recetario'
+
+
+#para jalar la configuracion de mi flask y extraer su conexion a la base de datos
+conexion.init_app(app)
+
+#create_all> con este comando indicaremos la creacion de todas las tablas en la bd
+#emitira un error si es que no hay ninguna tabla a crear
+#emitira un error si no le hemos instalado el conector correctamente
+#tenemos que declarar en el parametro app nuestra aplicacion de flask
+conexion.create_all(app=app)
 
 @app.route('/status', methods = ['GET'])
 def status():
@@ -14,6 +34,10 @@ def status():
         'status':True,
         'date': datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     }
+@app.route('/')
+def inicio():
+    return 'Bienvenido a mi Api de recetas'
+
 #Ahora definimos las rutas que van a ser utilizadas con un determinado controlador
 api.add_resource(IngredientesController,'/ingredientes','/ingrediente')
 
--- controllers/ingredientes.py
+++ controllers/ingredientes.py
@@ -1,13 +1,42 @@
 from flask_restful import Resource, request
+from config import conexion
+from models.ingredientes import Ingrediente
 #todos los metodos HTTP que vamos a utilizar se define como metodos
 #de la clase
 class IngredientesController(Resource):
     def get(self):
+        #sesion.query> vamos a crear una sesion en la cual ejecutaremos una query
+        resultado = conexion.session.query(Ingrediente).all()
+        print(resultado)
         return{
-            'message':'Yo soy el get de los ingredientes'
+            'message':'Yo soy el get de los ingredientes',
+            'content':{
+                'id': resultado[0].id,
+                'nombre':resultado[0].nombre
+
+            } 
         }
     def post(self):
         print(request.get_json())
-        return{
-            'message': 'yo soy el post'
-        }
+        #registramos un nuevo ingrediente
+        try:
+            nuevoIngrediente = Ingrediente()
+            nuevoIngrediente.nombre= 'Leche evaporada'
+            #ahora guardo la informacion en la base de datos
+            conexion.session.add(nuevoIngrediente)
+            #add> estamos creando una nueva transaccion
+            #commit> sirve para guardar los cambios de manera permanete en la bd
+            conexion.session.commit()
+
+            return{
+                'message': 'Ingrediente creado exitosamente'
+            }
+        except Exception as e:
+            print(e.args[0])
+            #si hubo un erros al momento de hacer alguna modificacion a la bd
+            #entonces 'retrocedemos' todos esas modificaciones y lo dejaremos sin ningun cambio
+            conexion.session.rollback()
+            return{
+                'message': 'Hubo un erro al crear el ingrediente',
+                'content': e.args[0]
+            }
