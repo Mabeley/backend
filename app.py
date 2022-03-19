fe642691cfc2607060bb0274c3606c436bc7b808
Mabeley <mabel.elcie@gmail.com>
2022-03-19 17:21:26 -0500

    creacion de dtos

--- app.py
+++ app.py
@@ -55,3 +55,5 @@ if __name__ == '__main__':
     app.run(debug=True)
 
 print('Hola')
+
+#2:18
--- controllers/ingredientes.py
+++ controllers/ingredientes.py
@@ -2,7 +2,8 @@ from flask_restful import Resource, request
 from config import conexion
 from models.ingredientes import Ingrediente
 from dtos.dto_prueba import ValidadorPrueba, ValidarUsuarioPrueba
-from dtos.ingrediente_dto import IngredienteRequestDTO
+from dtos.ingrediente_dto import IngredienteRequestDTO, IngredienteResponseDTO
+from marshmallow.exceptions import ValidationError
 #todos los metodos HTTP que vamos a utilizar se define como metodos
 #de la clase
 class IngredientesController(Resource):
@@ -10,12 +11,12 @@ class IngredientesController(Resource):
         #sesion.query> vamos a crear una sesion en la cual ejecutaremos una query
         resultado = conexion.session.query(Ingrediente).all()
         print(resultado)
+        #many= True>si nosotros queremos convertir la informacion y es mas de una
+        #con esto el DTO iterara cada uno de los items y lo serializara al valor correcto
+        ingredientesSerializados = IngredienteResponseDTO(many=True).dump(resultado)
         return{
             'message':'Yo soy el get de los ingredientes',
-            'content':{
-                'id': resultado[0].id,
-                'nombre':resultado[0].nombre
-                      } 
+            'content': ingredientesSerializados
                 }
     def post(self):
         print(request.get_json())
@@ -34,19 +35,26 @@ class IngredientesController(Resource):
             #add> estamos creando una nueva transaccion
             #commit> sirve para guardar los cambios de manera permanete en la bd
             conexion.session.commit()
-
+            ingredienteSerializado = IngredienteResponseDTO().dump(nuevoIngrediente)
             return{
-                'message': 'Ingrediente creado exitosamente'
-            }
+                'message': 'Ingrediente creado exitosamente',
+                'ingrediente': ingredienteSerializado
+            }, 201
+        except ValidationError as e:
+            return{
+                'message':'La informacion es incorrecta',
+                'content': e.args
+            },400 # bad request(mala solicitud)
         except Exception as e:
+            
             print(e.args[0])
             #si hubo un erros al momento de hacer alguna modificacion a la bd
             #entonces 'retrocedemos' todos esas modificaciones y lo dejaremos sin ningun cambio
             conexion.session.rollback()
             return{
-                'message': 'Hubo un erro al crear el ingrediente',
+                'message': 'Hubo un error al crear el ingrediente',
                 'content': e.args[0]
-            }
+            },500 #internal server error
 
 class PruebaController(Resource):
     def post(self):
