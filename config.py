277cb846c354cb69dc29e6119fc175124900a206
Mabeley <mabel.elcie@gmail.com>
2022-03-24 19:57:19 -0500

    limpieza de la rama

--- config.py
+++
@@ -1,7 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-from flask_marshmallow import Marshmallow
-
-validador = Marshmallow()
-conexion = SQLAlchemy()
-
-#todo el uso de todas las clases, se crea una sola instancia
