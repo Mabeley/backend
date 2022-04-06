cb20d78cef1c983db75db3d7e65b9cda6fd41f1c
Mabeley <mabel.elcie@gmail.com>
2022-04-05 22:38:32 -0500

    models

--- agenda/gestion/models.py
+++ agenda/gestion/models.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+from tkinter.tix import INTEGER
 from django.db import models
 
 # Create your models here.
@@ -21,3 +23,46 @@ class Etiqueta(models.Model):
         #modificar el ordenamiento natural (por el id)e imponiendo el propio que
         #seas ASC del nombre, solamente funcionara cuando hagamos el get usando el ORM
         ordering= ['-nombre']
+
+class Tareas(models.Model):
+
+    class CategoriaOpciones(models.TextChoices):
+        #cada opcion le podemos pasar dos parametros en la cual el primero sera su abreviatura
+        #para que se guarde en la bd y el segundo el nombre completo
+        TODO = 'TODO','TO_DO'
+        IN_PROGRESS = 'IP', 'IN_PROGRESS'
+        DONE = 'DONE', 'DONE'
+        CANCELLED = 'CANCELED', 'CANCELLED'
+
+
+    id= models.AutoField(primary_key=True)
+    nombre=models.CharField(max_length=45, null=False)
+    #Forma I usando una subclase que herede de TextChoices
+    categoria = models.CharField(max_length=45, choices =CategoriaOpciones.choices,
+    default=CategoriaOpciones.TODO)
+     #Forma II usando una lista de tuplas
+    # categoria =models.CharField(max_length=45, choices=[
+    #     ('TODO','TO_DO'),
+    #     ('IP','IN_PROGRESS')
+    #     ('DONE', 'DONE')
+    #     ('CANCELED', 'CANCELLED')
+    #     ], default='TODO')
+    fechaCaducidad= models.DateTimeField(db_column='fecha_caducidad')
+    importancia=models.IntegerField(null=False)
+    descripcion = models.TextField()
+    createdAt = models.DateTimeField(auto_now_add=True, db_column='created_at')
+    updatedAt = models.DateTimeField(auto_now=True, db_column='updated_at')
+
+    #En Django se puede utilizar las relaciones one-to -one, one-to-many o many-to-many
+    #para crear las relaciones entre las tablas, aca ya no es necesario usar las relashionship
+    #porque ya estan integradas dentro de la relacion
+    etiquetas =models.ManyToManyField(to=Etiqueta, related_name = 'tareas')
+    class Meta:
+        db_table = 'tareas'
+#si la tbala tareas_etiquetas no fuese una tabla pivote, detalle entonces tendria que 
+#que crear la tabla como si fuese una tabla comun y corriente
+#class TareasEtiquetas(models.Model):
+#...
+#etiquetasFK= models.ForeignKey(to=Etiqueta)
+#tareaFK=models.ForeignKey(to=Tareas)
+#las demas columnas
--- agenda/gestion/views.py
+++ agenda/gestion/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+#render> este render sirve para renderizar una plantilla html
+#from django.shortcuts import render
 
 # Create your views here.
+
+from rest_framework.decorators import api_view
+
+@api_view(http_method_names=['GET','POST'])
+def inicio(request):
+    #reqeuest sera toda la informacion enviada por el cliente
+    return {
+        'message': 'Bienvenido a mi API de agenda'
+    }
+        
