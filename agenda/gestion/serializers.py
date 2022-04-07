bfe1446f2e5edf8d08d8c5815eacc43f1bf22a6b
Mabeley <mabel.elcie@gmail.com>
2022-04-06 21:16:29 -0500

    creacion controladores

--- agenda/gestion/serializers.py
+++ agenda/gestion/serializers.py
@@ -1,8 +1,18 @@
-from rest_framework import serializer
-class PruebaSerializer(serializer.Serializer):
-    #https://www.django-rest-framework.org/api-guide/serializers
-    nombre = serializer.CharField(max_length=40, trim_whitespace=True)
-    apellido = serializer.CharField()
-    correo = serializer.EmailField()
-    dni= serializer.RegexField(max_length=8, min_length=8, regex="[0-9]")
+from rest_framework import serializers
+from .models import Tareas
+class PruebaSerializer(serializers.Serializer):
+    #https://www.django-rest-framework.org/api-guide/fields
+    nombre = serializers.CharField(max_length=40, trim_whitespace=True)
+    apellido = serializers.CharField()
+    correo = serializers.EmailField()
+    dni= serializers.RegexField(max_length=8, min_length=8, regex="[0-9]")
     #dni = serializer.IntegerField(min_value=10000000, max_value=99999999)
+
+
+class TareaSerializer(serializers.ModelSerializer):
+    #https://www.django-rest-framework.org/api-guide/serializers/#modelserializer
+    class Meta:
+        model =Tareas
+        fields ='__all__' #estare indicando que voy a utilizar todas las columnas de mi tabla
+        #exclude = ['importancia'] #indicara que columnas NO quiero utilizar
+        #NOTA: no se puede utilizar los dos a la vez, es decir, o bien se usa el fields o el exclude
--- agenda/gestion/views.py
+++ agenda/gestion/views.py
@@ -1,4 +1,4 @@
-#render> este render sirve para renderizar una plantilla html
+# render> este render sirve para renderizar una plantilla html
 #from django.shortcuts import render
 
 # Create your views here.
@@ -6,27 +6,66 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.request import Request
-from rest_framework.generics import ListAPIView
-@api_view(http_method_names=['GET','POST'])
+from rest_framework.generics import ListAPIView, ListCreateAPIView
+from .serializers import PruebaSerializer, TareaSerializer
+from .models import Tareas
+
+@api_view(http_method_names=['GET', 'POST'])
 def inicio(request: Request):
-    #reqeuest sera toda la informacion enviada por el cliente
-    #https://www.django-rest-framework.org/api-guide/requests
+    # reqeuest sera toda la informacion enviada por el cliente
+    # https://www.django-rest-framework.org/api-guide/requests
     print(request.method)
     print(request)
     if request.method == 'GET':
-        #comportamiento cuando sea GET
-        return Response(data ={
-        'message': 'Bienvenido a mi API de agenda'
+        # comportamiento cuando sea GET
+        return Response(data={
+            'message': 'Bienvenido a mi API de agenda'
         })
     elif request.method == 'POST':
-        #Comportamiento cuando sea POST
+        # Comportamiento cuando sea POST
 
-        return Response(data ={
+        return Response(data={
             'message': 'Hiciste un post'
-        }, status = 201)
-        
+        }, status=201)
 
 
 class PruebaApiView(ListAPIView):
-    serializer_class = None
-    
+    # sirve para ayudarnos cuando se llame este request nos haga el trabajo de
+    # serializar y de deserializar la informacion
+    # (es igual q un DTO)
+    serializer_class = PruebaSerializer
+    #queryset es el encargado de hacer la busqueda para este controlador(para todos sus metodos )
+    queryset = [{'nombre': 'Mabel',
+                'apellido': 'Cisneros',
+                 'correo': 'mabel_elcie@hotmail.com',
+                 'dni': '25974365',
+                 'estado_civil': 'soltera'},
+                 {'nombre': 'Alex',
+                'apellido': 'suarez',
+                 'correo': 'ale.sdjfe@hotmail.com',
+                 'dni': '87596412',
+                 'estado_civil': 'viudo'}
+                 ]
+
+    def get(self, request:Request):
+        #dentro de las vistas genericas se puede sobrescribir la logica
+        #inicial del controlador
+        #si modifico la logica original de cualquier generico en base a su
+        #metodo a utilizar ya no sera necesario definir los atributos
+        #serializer_class y queryset ya que estos se usan para cuando no
+        #se modifica la logica original
+
+        informacion= self.queryset
+        #uso el serializador para filtar la infofmacion necesaria y no mostrar 
+        #alguna informacion de mas pero en este caso como le voy a pasar uno
+        #o mas registrso de usuario, para q el serializador los pueda serializar
+        #se coloca el parametro many)True que lo que hara sera iterar
+        informacion_serializada = self.serializer_class(data =informacion, many=True)
+        informacion_serializada.is_valid(raise_exception=True)
+        return Response(data={
+            'message':'Hola',
+            'content': informacion_serializada.data
+            })
+class TareasApiView(ListCreateAPIView):
+    queryset =Tareas.objects.all() #> esto es igual a SELECT * FROM tareas;
+    serializer_class= TareaSerializer
